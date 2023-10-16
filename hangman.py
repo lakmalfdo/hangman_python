e1a43a1db780fbaa6f32b5897dd17cd4661d7193
Lakmal Fernando <lakmalfdo@gmail.com>
2023-10-16 15:34:50 +0530

    Developed a Hangman game with ASCII art, word list, and game logic

---
+++ hangman.py
@@ -0,0 +1,156 @@
+import random
+
+# List of words for the Hangman game
+word_list = ["elephant", "lion", "tiger", "giraffe", "zebra", "kangaroo", "penguin", "dolphin", "rhinoceros", "octopus", "cheetah", "koala", "hedgehog", "crocodile", "seagull", "panda", "otter", "polarbear", "platypus", "chimpanzee"]
+
+# Function to display the hangman stage in ASCII art
+def display_hangman_stage():
+    hangman_stages = [
+"""
+________    
+|    |    
+|         
+|         
+|         
+|         
+=========
+""",
+"""
+________    
+|    |    
+|    O    
+|         
+|         
+|         
+=========
+""",
+"""
+________    
+|    |    
+|    O    
+|    |    
+|         
+|         
+=========
+""",
+"""
+________    
+|    |    
+|    O    
+|   /|    
+|         
+|         
+=========
+""",
+"""
+________    
+|    |    
+|    O    
+|   /|\\  
+|         
+|         
+=========
+""",
+"""
+________    
+|    |    
+|    O    
+|   /|\\  
+|   /     
+|         
+=========
+""",
+"""
+________    
+|    |    
+|    O    
+|   /|\\  
+|   / \\  
+|         
+=========
+"""
+    ]
+    print(hangman_stages[attempts])
+
+# Select a random word from the list
+word = random.choice(word_list)
+
+# Create a list to store the guessed letters
+guessed_letters = []
+
+# Function to display the current state of the word
+def get_answer():
+    answer = ""
+    for letter in word:
+        if letter in guessed_letters:
+            answer += letter
+        else:
+            answer += "_"
+    return answer
+
+# Maximum number of incorrect guesses allowed
+max_attempts = 6
+
+attempts = 0
+
+# Display Hangman game name in ASCII art
+print("  _    _")
+print(" | |  | |")
+print(" | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __")
+print(" |  __  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\")
+print(" | |  | | (_| | | | | (_| | | | | | | (_| | | | |")
+print(" |_|  |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|")
+print("                      __/ /")
+print("                     |___/")
+print()
+print("Guess the Animal")
+
+# Main game loop
+while True:
+    display_hangman_stage()
+    print(get_answer())
+
+    while True:
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You've already guessed that letter.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess not in word:
+            attempts += 1
+
+        display_hangman_stage()
+        print(get_answer())
+
+        if get_answer() == word:
+            print("Congratulations, you've won!")
+            break
+
+        if attempts >= max_attempts:
+            print("Game Over. The word was:", word)
+            break
+
+    while True:
+        play_again = input("Do you want to play again? (y/n): ").strip().lower()
+
+        if play_again in ("y", "yes"):
+            # Select a new random word from the list
+            word = random.choice(word_list)
+            # Clear guessed letters from previous game
+            guessed_letters.clear()
+            attempts = 0
+            break
+        elif play_again in ("n", "no"):
+            print("Thank you for playing Hangman. Goodbye!")
+            exit()
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+            continue
+
